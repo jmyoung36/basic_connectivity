3a171e75f612e081c4fa5771c3823a3460d2f411
Jonathan Young <jonyoung@Jonathans-MacBook-Air.local>
2016-08-26 12:30:22 +0100

    finished run_covInfGeo and added run_covArsigny

--- run_covInfGeoE.m
+++ run_covInfGeoE.m
@@ -1,8 +1,8 @@
 % set data directory
 % for work desktop machine
-data_dir = '/home/jonyoung/IoP_data/Data/connectivity_data/';
+%data_dir = '/home/jonyoung/IoP_data/Data/connectivity_data/';
 % for macbook air
-%data_dir = '/Users/jonyoung/Data/Connectivity data/';
+data_dir = '/Users/jonyoung/Data/Connectivity data/';
 
 % read in some data
 data = csvread([data_dir, 'M_timecourse_connectivity_data.csv']);
@@ -10,7 +10,7 @@ labels = data(:, 1);
 connectivity_data = data(:, 2:end);
 
 % optional - only include positive connections
-connectivity_data(connectivity_data < 0) = 0;
+%connectivity_data(connectivity_data < 0) = 0;
 
 % correct labels for GPML
 labels(labels == 0) = -1;
@@ -34,6 +34,9 @@ for i = 1:size(connectivity_data,1);
     end    
 end
 
+% to hold predicted probabilities
+ps = zeros(length(labels), 1);
+
 % get a set of k-fold cross-validations
 k = 10;
 kf = kFold(length(labels), k);
@@ -45,10 +48,9 @@ for i = 1:k
     
     training_indices = kf(i).training_indices;
     testing_indices = kf(i).testing_indices;
-    %K_train = {K(training_indices, training_indices)};
-    %K_test = {K(testing_indices, training_indices)};
-    K_train = {connectivity_data(training_indices, :) * connectivity_data(training_indices, :)'};
-    K_test = {connectivity_data(testing_indices, :) * connectivity_data(training_indices, :)'};
+    K_train = {K(training_indices, training_indices)};
+    K_cross = {K(training_indices, testing_indices)};
+    K_test = {K(testing_indices, testing_indices)};
     training_labels = labels(training_indices);
     testing_labels = labels(testing_indices);
     
@@ -57,13 +59,26 @@ for i = 1:k
     
     % set up GP
     meanfunc = @meanConst_K; hyp.mean = 0;
-    covfunc = @covLINMKL; hyp.cov = [0 0];
+    covfunc = @covSEMKL; hyp.cov = [log(mean_dist) 0 0];
     likfunc = @likErf;
-    %hyp = minimize(hyp, @gp_K, -40, @infEP_K, meanfunc, covfunc, likfunc, K_train, training_labels);
     hyp_opt = minimize(hyp, @gp_K, -200, @infEP_K, covfunc, likfunc, meanfunc, K_train, training_labels);
-
-    
+    [a b c d lp post] = gp_K(hyp_opt, @infEP_K, covfunc, likfunc, meanfunc, K_train, training_labels, K_test, K_cross, ones(length(testing_labels), 1));
+    ps(testing_indices) = exp(lp);
+    preds = exp(lp);
+    preds(preds > 0.5) = 1;
+    preds(preds < 0.5) = -1;
+    [acc, sens, spec] = accStats(testing_labels, preds);
+    acc
+    sens
+    spec
 end
+
+ps(ps > 0.5) = 1;
+ps(ps < 0.5) = -1;
+[acc, sens, spec] = accStats(labels, ps);
+acc
+sens
+spec
 
 
 
